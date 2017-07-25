01860d7a65f7c4dbeeedd9fcce0d151e690636fd
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-07-25 00:24:21 -0500

    worked on getting the front page figured out

---
+++ template-parts/panels/content-front.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for displaying page content on the front-page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage twentyseventeen-child-LAW
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<div>
+  <h1>content-front panels</h1>
+
+  <?php the_content() ?>
+
+</div>
