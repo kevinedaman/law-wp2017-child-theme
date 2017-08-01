aaea1a135f60af88b6241a9de5ee55cd2f3f3918
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-07-31 23:29:10 -0500

    started adding walker

--- functions.php
+++ functions.php
@@ -53,76 +53,17 @@ function twentyseventeen_child_LAW_scripts () {
 
 add_action( 'wp_enqueue_scripts', 'twentyseventeen_child_LAW_scripts' );
 
-/**
- * Create a nav menu with very basic markup.
- *
- * @author Thomas Scholz http://toscho.de
- * @version 1.0
- */
-class T5_Nav_Menu_Walker_Simple extends Walker_Nav_Menu
-{
-	/**
-	 * Start the element output.
-	 *
-	 * @param  string $output Passed by reference. Used to append additional content.
-	 * @param  object $item   Menu item data object.
-	 * @param  int $depth     Depth of menu item. May be used for padding.
-	 * @param  array $args    Additional strings.
-	 * @return void
-	 */
-	public function start_el( &$output, $item, $depth, $args )
-	{
-		$output     .= '<li>';
-		$attributes  = '';
-		! empty ( $item->attr_title )
-			// Avoid redundant titles
-			and $item->attr_title !== $item->title
-			and $attributes .= ' title="' . esc_attr( $item->attr_title ) .'"';
-		! empty ( $item->url )
-			and $attributes .= ' href="' . esc_attr( $item->url ) .'"';
-		$attributes  = trim( $attributes );
-		$title       = apply_filters( 'the_title', $item->title, $item->ID );
-		$item_output = "$args->before<a $attributes>$args->link_before$title</a>"
-						. "$args->link_after$args->after";
-		// Since $output is called by reference we don't need to return anything.
-		$output .= apply_filters(
-			'walker_nav_menu_start_el'
-			,   $item_output
-			,   $item
-			,   $depth
-			,   $args
-		);
-	}
-	/**
-	 * @see Walker::start_lvl()
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @return void
-	 */
-	public function start_lvl( &$output )
-	{
-		$output .= '<ul class="sub-menu">';
-	}
-	/**
-	 * @see Walker::end_lvl()
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @return void
-	 */
-	public function end_lvl( &$output )
-	{
-		$output .= '</ul>';
-	}
-	/**
-	 * @see Walker::end_el()
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @return void
-	 */
-	function end_el( &$output )
-	{
-		$output .= '</li>';
-	}
+class Bootstrap_Nav_Walker extends Walker_Nav_Menu {
+
+  function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
+    	$object = $item->object;
+    	$type = $item->type;
+    	$title = $item->title;
+    	$description = $item->description;
+    	$permalink = $item->url;
+
+  }
+
 }
 
 ?>
