f8039311d21f39bac38649505919ade61bb13551
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-06 18:10:33 -0500

    things are back to normal

--- functions.php
+++ functions.php
@@ -56,11 +56,7 @@ function twentyseventeen_child_LAW_scripts () {
 	wp_enqueue_style( 'font-awesome', get_theme_file_uri() . '/includes/css/font-awesome.min.css', false, '4.1.0' );
 
 	// load styleshet
-  wp_enqueue_style( 'child-style',
-      get_stylesheet_directory_uri() . '/style.css',
-      array( $parent_style ),
-      wp_get_theme()->get('Version')
-  );
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
 
   wp_deregister_script( 'jquery' );
 
